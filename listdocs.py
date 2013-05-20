71f7783f8f41410db9559d7314db53dcae127c66
Chris Hedrick <christopher.m.hedrick@gmail.com>
2013-05-20 17:58:00 -0400

    hardcoded username, commands will now be done as methods

--- listdocs.py
+++ listdocs.py
@@ -1,13 +1,25 @@
 import gdata.docs.service as docserv
 import getpass
 
-username = raw_input('username: ')
+#username = raw_input('username: ')
+username = 'christopher.m.hedrick'
 password = getpass.getpass('password: ')
 client = docserv.DocsService()
 client.ClientLogin(username, password)
 documents_feed = client.GetDocumentListFeed()
 
-for document_entry in documents_feed.entry:
-    print document_entry.title.text
-
-
+def get_list():
+    for document_entry in documents_feed.entry:
+        print document_entry.title.text
+    
+command = ''
+while command != 'q':
+    print '\n'
+    print 'Commands:'
+    print 'q = quit'
+    print 'g = get'
+    print 's = something'
+    command = raw_input('Command: ')
+    print '\n'
+    if 'g':
+        get_list()
