4a5b6c3aeef99388a118638cdb8ae6a552795666
Chris Hedrick <christopher.m.hedrick@gmail.com>
2013-06-05 13:45:24 -0400

    insert rows now possible, set function added to work on specific spreadsheets at a time

--- listdocs.py
+++ listdocs.py
@@ -1,6 +1,7 @@
 import gdata.docs.service as docserv
 import gdata.spreadsheet.service as spreadserv
 import getpass
+import re
 
 #username = raw_input('username: ')
 username = 'christopher.m.hedrick'
@@ -13,6 +14,7 @@ spreadclient.password = password
 spreadclient.ProgrammaticLogin()
 spreadsheet_feed = spreadclient.GetSpreadsheetsFeed()
 documents_feed = client.GetDocumentListFeed()
+set_spreadid = ''
 
 def get_list():
     for document_entry in documents_feed.entry:
@@ -35,16 +37,43 @@ def get_spreadsheet_ids():
         spread_needs = zip(spread_keys, spread_ids)
     for key, value in spread_needs:
         out.append(' ' + str(key) + ' ' + str(value))
-    print_spreadsheets_ids(out)
+    return out
 
-def print_spreadsheets_ids(out):
+def print_spreadsheets_ids(processed_ids):
     for i in range(len(spreadsheet_feed.entry)):
-        print spreadsheet_feed.entry[i].title.text + out[i]
+        print spreadsheet_feed.entry[i].title.text + processed_ids[i]
 
 def sheet_select():
     feed = spreadclient.GetListFeed(spread_code, worksheet_id)
     for row in feed.entry:
         print "%s| %s\n" % (row.title.text, row.content.text)
+
+def set_spreadsheet(command):
+    working_sheet = command.split()
+    global set_spreadid
+    set_spreadid = working_sheet[1]
+    global set_workid
+    set_workid = working_sheet[2]
+
+def make_columns():
+    feed = spreadclient.GetListFeed(set_spreadid, set_workid)
+    splitted_columns = feed.entry[0].custom.items()
+    col = []
+    for i in range(len(splitted_columns)):
+        col.append(splitted_columns[i][0])
+    return col
+
+def enter_columns(cols):
+    new_row = {}
+    for col in cols:
+        new_row[col] = raw_input(col + ' ')
+    return new_row
+
+def insert_row(row):
+    spreadclient.InsertRow(row, set_spreadid, set_workid)
+
+def test():
+    import pdb; pdb.set_trace()
 
 command = ''
 while command != 'q':
@@ -53,17 +82,29 @@ while command != 'q':
     print 'g = get'
     print 's = spreadsheet ids'
     print 'select [spreadsheet id] [worksheet id] = read worksheet'
+    print 'set [spreadsheet id] [worksheet id] = set a worksheet to work on'
+    print 'insert = insert row'
     command = raw_input('Command: ')
     print '------------------------'
     if command == 'g':
         get_list()
     elif command == 's':
-        get_spreadsheet_ids()
+        processed_ids = get_spreadsheet_ids()
+        print_spreadsheets_ids(processed_ids)
+    elif command == 't':
+        test()
     elif 'select' in command.split():
         selected = command.split()
         spread_code = selected[1]
         worksheet_id = selected[2]
         sheet_select()
+    elif 'insert' in command:
+        cols = make_columns()
+        row = enter_columns(cols)
+        insert_row(row)
+
+    elif 'set' in command:
+        set_spreadsheet(command)
     elif command == 'q':
         break
     else:
